252f71c3e2ac287dca4500f59a9edf0e79cb4756
VishnuBhargav2000 <40735102+VishnuBhargav2000@users.noreply.github.com>
2021-04-18 20:31:50 +0530

    reservations implemented

--- main.py
+++ main.py
@@ -1,40 +1,126 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS, cross_origin
 import random
+import RPi.GPIO as GPIO
+import time
+import asyncio
+import websockets
+from datetime import datetime, timedelta 
+
+
+# setting pins for pi
+triggers = [7,13]  # Trigger pins
+echos = [11,24]  # Echo pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
 app = Flask(__name__)
 
 app.config[ 'SECRET_KEY' ] = 'secret'
 socketio = SocketIO( app )
 
-def sendData():
-	while True:
-		distance = random.randint(3, 9)
-		if distance > 5:
-			isThere = 'FT'
-		else:
-			isThere = 'TF'
 
-		socketio.emit('spots', isThere)
-		socketio.sleep(2)
-	
+
+def GetDistance(TRIG,ECHO):
+    # driver code for ultrasonic sensor thats connected to raspberry pi
+    # returns a string (True/False) based on the distance data accquired from the sensor
+    # True = if distance greater than a pre defined value, False if lesser. which indicates if a spot is taken or available
+    heightofLot = 10
+    isThere = 'T'
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+
+    GPIO.output(TRIG, False)
+    time.sleep(0.2)
+
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    pulse_duration = pulse_duration/2
+    distance = pulse_duration * 34000
+    distance = round(distance, 2)
+
+    if distance > heightofLot:
+        isThere = 'F'
+    else:
+        isThere = 'T'
+
+    return isThere
+
+
+class SPSDriverClass:
+    def __init__(self,triggerPins, echoPins):
+        self.lots = [0,0,0,0,0,0]
+        self.reservedLots = {0: None, 1: None, 2: None, 3: None, 4: None, 5: None}
+        self.sensors = 2
+        self.tempInfo = ''
+        self.echoPins = echoPins
+        self.triggerPins = triggerPins
+        self.reserveTime = 1
+
+
+    def parkingInfo(self):
+            self.tempInfo = ''
+            for sensor in range(self.sensors):
+                self.tempInfo += GetDistance(self.triggerPins[sensor],self.echoPins[sensor])
+
+    def reserveLot(self, lotId):
+        self.lots[lotId] = 1
+        self.reservedLots[lotId] = datetime.now() + timedelta(minutes = self.reserveTime)
+        
+
+    def getFinalValue(self):
+        self.parkingInfo()
+        self.validateReservations()
+        for lot in range(len(self.lots)):
+            if self.lots[lot] == 1:
+                self.tempInfo = self.tempInfo[:lot]+'T'+self.tempInfo[lot+1:]
+        return self.tempInfo
+
+    def validateReservations(self):
+        now = datetime.now()
+        for slot in self.reservedLots.keys():
+            if self.reservedLots[slot] is not None:
+                if now > self.reservedLots[slot]:
+                    self.lots[slot] = 0
+            else:
+                pass
+
+def communicate():
+    while True:
+        spotsOccupied = SPS.getFinalValue()
+        socketio.emit('spots', spotsOccupied)
+        socketio.sleep(1)
 
 @app.route('/')
 def index():
-	return render_template('./index.html')
+    return render_template('./index.html')
 
 @socketio.on('connected')
 def messageRecived(msg):
-  print( '+++++++++++++NEW CONNECTION HAS BEEN MADE+++++++++++++' )
+  print( 'connection from ' + msg + "made")
 
-@socketio.on('send_req')
-def handler(msg):
-	sendData()
-	
-@socketio.on('connected')
-def messageRecived(msg):
-  print( '+++++++++++++NEW CONNECTION HAS BEEN TERMINATED+++++++++++++' )
+@socketio.on('startCommunication')
+def communicationHandler(msg):
+    print("communicate called from :" + msg)
+    communicate()
 
+@socketio.on('reserveRequest')
+def reservationHandler(msg):
+    print("reserveRequest by spot ",msg)
+    SPS.reserveLot(msg)
+    
+
+SPS = SPSDriverClass(triggers,echos)
 
 if __name__ == '__main__':
-  socketio.run( app, port = 9999, debug = True )
+    cors = CORS(app,resources={r"/*":{"origins":"*"}})
+    socketio.run( app,host="192.168.0.7", port = 9999, debug = True)
--- static/scripts.js
+++ static/scripts.js
@@ -1,13 +1,19 @@
-var socket = io.connect('http://127.0.0.1:9999/');
+var socket = io.connect('http://192.168.0.7:9999/');
+var spotIndex = [0,0,0,0,0,0]
+var reserve = -1
 
 socket.on('connect', function(){
-    socket.emit('connected', '');
+    socket.emit('connected', 'client test 1');
     start_comm()
 })
 
+
+
 function start_comm(){
-	socket.emit('send_req', '');
+	socket.emit('startCommunication', 'client test 1');
 }
+
+
 
 socket.on('spots', function feast(msg){
 	console.log(msg)
@@ -15,32 +21,23 @@ socket.on('spots', function feast(msg){
     else{ document.getElementById("spot1").style.backgroundColor = "red";}
 
     if (msg[1] == 'T') { document.getElementById("spot2").style.backgroundColor = "green";}
-	else{document.getElementById("spot2").style.backgroundColor = "red";}	})
-/*
-var spotIndex = [0,0,0,0,0,0]
-var reserve = -1
-
-ws.onmessage = function (event) {     
-    if (event.data[0] == 'T') { document.getElementById("spot1").style.backgroundColor = "green";}
-    else{ document.getElementById("spot1").style.backgroundColor = "red";}
-
-    if (event.data[1] == 'T') { document.getElementById("spot2").style.backgroundColor = "green";}
 	else{document.getElementById("spot2").style.backgroundColor = "red";}
 
-	sendMessage();
+	if (msg[2] == 'T') { document.getElementById("spot3").style.backgroundColor = "green";}
+	else{document.getElementById("spot3").style.backgroundColor = "red";}
 
-		 };
+	if (msg[3] == 'T') { document.getElementById("spot4").style.backgroundColor = "green";}
+	else{document.getElementById("spot4").style.backgroundColor = "red";}
 
-function sendMessage() {
-	for (var i = 0; i < spotIndex.length; i++) {
-		if(spotIndex[i] == 1){
-			reserve = i;
-			spotIndex[i] = 0;}
-		}
-		ws.send(reserve);
-		reserve = -1
+	if (msg[4] == 'T') { document.getElementById("spot5").style.backgroundColor = "green";}
+	else{document.getElementById("spot5").style.backgroundColor = "red";}
+
+	if (msg[5] == 'T') { document.getElementById("spot6").style.backgroundColor = "green";}
+	else{document.getElementById("spot6").style.backgroundColor = "red";}
+
+})
+
+function reserveSpot(number){
+	socket.emit('reserveRequest', number);
 }
 
-function reserveSpot(spotId) {
-	spotIndex[spotId] = 1;
-}*/
